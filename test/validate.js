d3663ff6a3704ba9091d022fcade0cf54c03f3ea
Matthijs van Henten <matthijs@ischen.nl>
2013-05-14 23:09:21 +0200

    improved test cases

--- test/validate.js
+++ test/validate.js
@@ -1,13 +1,161 @@
-var assert = require('assert'),
-    random = require('./lib/random'),
+var _ = require('lodash'),
+    assert = require('assert'),
     format = require('util').format,
+    random = require('./lib/random'),
     Validate = require('../');
 
 
 suite('validate-arguments', function() {
+    test('Validate must be gracefull about bad input', function(done) {
+        var cases = [
+            {
+                'functions': [
+                    function() {},
+                    function() {}]
+            },
+            {
+                'null and null': [null, null]
+            },
+            {
+                'empty objects': [{}, {}]
+            },
+            {
+                'empty arrays': [[], []]
+            },
+            {
+                'undefs': []
+            },
+        ];
+
+        cases.forEach(function(testCase) {
+            var label = _.first(_.keys(testCase)),
+                values = testCase[label];
+
+            assert.ok(label && values, 'we have a case');
+
+            assert.doesNotThrow(function() {
+                var valid = Validate.validateObject(values[0], values[1]);
+
+                assert.equal(valid.isValid(), false, 'invalid input is not valid');
+            });
+
+            assert.doesNotThrow(function() {
+                var valid = Validate.validatePositional(values[0], values[1]);
+
+                assert.equal(valid.isValid(), false, 'invalid input is not valid');
+            });
+        });
+
+        done();
+    });
+
+    test('Validate returns a validationObject', function(done) {
+        var value = random.string(),
+            valid = Validate.validateObject({
+                str: value
+            }, {
+                str: {
+                    isa: 'string'
+                }
+            });
+
+        assert.ok(valid.isValid(), 'isValid returns a true value');
+        assert.equal(valid.get('str'), value, 'get returns expected value');
+        assert.equal(valid.errorString(), '', 'Error string is an empty string');
+        assert.deepEqual(valid.errors(), [], 'Errors is an emtpy array');
+        assert.deepEqual(valid.values(), [value], 'Values returns the values of the object');
+
+        valid = Validate.validatePositional([value], [{
+                isa: 'string'
+            }]);
+
+        assert.ok(valid.isValid(), 'isValid returns a true value');
+        assert.equal(valid.get(0), value, 'get returns expected value');
+        assert.equal(valid.errorString(), '', 'Error string is an empty string');
+        assert.deepEqual(valid.errors(), [], 'Errors is an emtpy array');
+        assert.deepEqual(valid.values(), [value], 'Values returns the values of the object');
+
+        done();
+    });
+
+    test('Validate returns a meaning ful errorString', function(done) {
+        var value = random.string();
+
+        var cases = [
+            {
+                label: 'Named arguments missing',
+                args: {},
+                spec: {},
+                expect: 'missing named arguments'
+            },
+            {
+                label: 'Missing arguments',
+                args: {
+                    bar: 1
+                },
+                spec: {
+                    foo: {
+                        isa: 'string'
+                    }
+                },
+                expect: 'missing named argumnent foo'
+            },
+            {
+                label: 'Multiple missing arguments',
+                args: {
+                    bar: 1
+                },
+                spec: {
+                    foo: {
+                        isa: 'string'
+                    },
+                    biz: {
+                        isa: 'number'
+                    }
+                },
+                expect: 'missing named argumnent foo, missing named argumnent biz'
+            },
+            {
+                label: 'Named argument is not a',
+                args: {
+                    foo: value
+                },
+                spec: {
+                    foo: {
+                        isa: 'number'
+                    }
+                },
+                expect: 'named argument foo is not a "number"'
+            },
+            {
+                label: 'Mixing errors',
+                args: {
+                    foo: value
+                },
+                spec: {
+                    foo: {
+                        isa: 'number'
+                    },
+                    biz: {
+                        isa: 'string'
+                    }
+                },
+                expect: 'named argument foo is not a "number", missing named argumnent biz'
+            }
+        ];
+
+        cases.forEach(function(testCase) {
+            var valid = Validate.validateObject(testCase.args, testCase.spec);
+            assert.equal(valid.isValid(), false);
+            assert.equal(valid.errorString(), testCase.expect);
+        });
+
+        done();
+    });
+
+
     test('validate basic types', function(done) {
-
-
+        // todo generalize test cases for types
         var cases = [{
                 label: 'boolean true is a boolean',
                 value: true,
